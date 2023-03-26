0947c5e0489d4c728950ef75ce3e871c6767beeb
rojih reda <rojihroja@gmail.com>
2023-03-26 12:25:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * main - check the code
- * print_alphabet - print all alphabet in lowercase
- * Return: Always 0.
+ * Descripion: print_alphabet print all alphabelt in lowercase
+ * Return: Always (0)
  */
 
 void print_alphabet(void)
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+ /**
+  * print_alphabet_x10 - print 10 times the al[habet, in lowercase 
+  */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (countt++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n");
+	}

fa96fa6a40ae7af2e8d8af55d3adcce9cee0ab39
rojih reda <rojihroja@gmail.com>
2023-03-26 11:59:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include "main.h"
-void print_alphabet(void);
 /**
  * main - check the code
  * alphabet function fot print letter alphabet.
  * Return: Always 0.
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
 void print_alphabet(void)
 {
 	char letter;

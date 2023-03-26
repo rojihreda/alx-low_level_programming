678d6ccaf31fb8975fee4ec27d490d52d2f1fe09
rojih reda <rojihroja@gmail.com>
2023-03-26 12:37:40 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,10 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-	while (countt++ <= 9)
+	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('\n");
+		_putchar('\n');
 	}
+}

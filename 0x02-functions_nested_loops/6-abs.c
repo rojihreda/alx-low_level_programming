83a6a9f3ee5408a053ed13ecbcadb81388a59e35
rojih reda <rojihroja@gmail.com>
2023-05-15 11:45:02 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * _abs - A function that makes a negative number positive
+ *
+ * @x: the num to into positive
+ *
+ *
+ * Return: return 'x'
+ */
+
+int _abs(int x)
+{
+	return (abs(x));
+}

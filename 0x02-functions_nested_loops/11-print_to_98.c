9d9d4f7a382a351daf318269981a3a90d97e7681
rojih reda <rojihroja@gmail.com>
2023-05-17 11:45:07 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_to_98 - function to print numbers from 0 to 98
+ *
+ * @n: number
+ *
+ * Return: return 'n'
+ *
+ */
+
+void print_to_98(int n)
+{
+	int x;
+
+	if (n < 98)
+	{
+		for (x = n; x < 98; x++)
+
+			printf("%d, ", x);
+	}
+	else
+	{
+		for (x = n; x > 98; x--)
+
+			printf("%d, ", x);
+	}
+}

27daa7a68afae18b54f574bf6b11df8a93578736
rojihreda <rojihroja@gmail.com>
2023-06-13 14:02:48 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - function to return 1 if input integr else return 0
+ * @n: number
+ * @othrn:int
+ * Return: int
+ */
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - check all number
+ * @n: int
+ * @othrn: int
+ * Return: int
+ */
+int check_prime(int n, int othrn)
+{
+	if (othrn >= n > 1)
+		return (1)'
+	else if (n % othrn == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, othrn + 1));
+}

dcc3ab636275b9c22ff18fe84401cff30cc2e56d
rojihreda <rojihroja@gmail.com>
2023-06-13 13:09:26 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function to print string revers
+ *
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
